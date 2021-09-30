5780233d82c8eb2682625e6499d7990b661ec4da
rafaelzolondek <rafaelzolondek@outlook.com>
2021-09-30 15:34:10 -0300

    comitando projeto de um player

---
+++ radio-ragatanga/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title:
+      "Положение",
+    artist: "DIOR",
+    cover: "DIOR - Положение.jpg",
+    file: "DIOR - Положение.mp3"
+  },
+  {
+    title: "What's Up Danger",
+    artist: "Blackway & Black Caviar",
+    cover: "What s Up Danger.png",
+    file: "What s Up Danger.mp3"
+  },
+  {
+    title: "Home",
+    artist: "Edith Whiskers",
+    cover: "Edith Whiskers - Home.jpg",
+    file: "Edith Whiskers - Home.mp3"
+  }
+];
+
